648f36b63dc9823f014888939de0efff8d5aab96
Oleg Bravo <oleg.bravo1853@gmail.com>
2024-07-20 13:55:26 +0200

    feat: add base styles

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import '@/assets/styles/nullstyle.scss';
+import '@/assets/styles/style.scss';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
