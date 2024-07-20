15c9f2feda1dfcaad18417681b0681e84b2a0340
Oleg Bravo <oleg.bravo1853@gmail.com>
2024-07-20 16:47:54 +0200

    build: add Quasar + MDI

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue';
+import { Quasar } from 'quasar';
+import quasarIconSet from 'quasar/icon-set/mdi-v7'
 import App from './App.vue';
+import 'quasar/dist/quasar.css';
+import '@quasar/extras/mdi-v7/mdi-v7.css'
 import '@/assets/styles/nullstyle.scss';
 import '@/assets/styles/style.scss';
 
-createApp(App).mount('#app');
+createApp(App).use(Quasar, {
+    iconSet: quasarIconSet
+}).mount('#app');
