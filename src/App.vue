8bccb695fc7b2b469325c71ec540c07bdc2b66f4
Oleg Bravo <oleg.bravo1853@gmail.com>
2024-07-23 19:00:03 +0200

    feat: add MainBlock.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <AppHeader />
+    <MainBlock />
   </div>
 </template>
 
 <script setup>
 import AppHeader from '@/components/AppHeader.vue';
+import MainBlock from '@/components/MainBlock.vue';
 </script>
-
-<style scoped></style>
---
+++ src/components/MainBlock.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="background-image">
+    <img src="@/assets/images/main-background.jpg" alt="Темный фон" />
+  </div>
+  <div class="main-container">
+    <div class="welcome-block">
+      <Transition name="text">
+        <div class="welcome-text">
+          <div>Привет! Меня зовут <span class="red-text">Олег</span></div>
+          <div>Занимаюсь Frontend</div>
+          <div>разработкой с 2021 года</div>
+        </div>
+      </Transition>
+      <div class="welcome-image">
+        <img src="@/assets/images/photo.png" alt="Фото" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+@import url(@/assets/styles/MainBlock.scss);
+</style>
