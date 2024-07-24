d76f14a3ecd0a4713397850d5d741eace9d9fbb3
Oleg Bravo <oleg.bravo1853@gmail.com>
2024-07-24 16:59:47 +0200

    feat: update adaptive MainBlock.vue

--- src/components/MainBlock.vue
+++ src/components/MainBlock.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="background-image">
-    <img src="@/assets/images/main-background.jpg" alt="Темный фон" />
-  </div>
-  <div class="main-container">
-    <div class="welcome-block">
-      <Transition name="text">
-        <div class="welcome-text">
-          <div>Привет! Меня зовут <span class="red-text">Олег</span></div>
-          <div>Занимаюсь Frontend</div>
-          <div>разработкой с 2021 года</div>
+  <section class="main-block">
+    <div class="background-image">
+      <img src="@/assets/images/main-background.jpg" alt="Темный фон" />
+    </div>
+    <div class="main-container">
+      <div class="welcome-block">
+        <Transition name="text">
+          <div class="welcome-text">
+            <div>Привет! Меня зовут <span class="red-text">Олег</span></div>
+            <div>Занимаюсь Frontend</div>
+            <div>разработкой с 2021 года</div>
+          </div>
+        </Transition>
+        <div class="welcome-image">
+          <img src="@/assets/images/photo.png" alt="Фото" />
         </div>
-      </Transition>
-      <div class="welcome-image">
-        <img src="@/assets/images/photo.png" alt="Фото" />
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script setup></script>
